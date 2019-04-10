fde7f7a41f19f5a52e698b234adc3ee4edc43bca
Owee Penaranda <c.m.penaranda@hotmail.com>
2019-04-10 20:35:16 +0800

    Draft for automated publishing in spo

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,66 @@
 
 const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
+// const spsync = require('gulp-spsync-creds').sync;
+
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
-build.initialize(gulp);
+const production = {
+    "username": "cromwel.m.penaranda@accenturemanilapdc.onmicrosoft.com",
+    "password": "Avanade1234",
+    "tenant": "accenturemanilapdc",
+    "cdnSite": "sites/AkuminaAddIn/",
+    "cdnLib" : "AppCatalog",
+    "catalogSite": "<catalog-site-relative-path>"
+}
 
-// const sppkgDeploy = require('node-sppkg-deploy');
-// sppkgDeploy.deploy({
-//     username: "cromwel.m.penaranda@accenturemanilapdc.onmicrosoft.com", // The user that will deploy the file
-//     password: "Avanade1234", // The password of the user
-//     tenant: "tenant", // The tenant name. Example: contoso
-//     absoluteUrl: "https://accenturemanilapdc.sharepoint.com/sites/AkuminaAddIn/AppCatalog", // Absolute path to the app catalog site
-//     skipFeatureDeployment: true, // Do you want to skip the feature deployment (SharePoint Framework)
-//     //filename: "akumina_all_in_one_package.sppkg", // Filename of the package
-//     verbose: true // Do you want to show logging during the deployment
-// });
+const test = {
+    "username": "cromwel.m.penaranda@accenturemanilapdc.onmicrosoft.com",
+    "password": "Avanade1234",
+    "tenant": "accenturemanilapdc",
+    "cdnSite": "sites/AkuminaAddIn/",
+    "cdnLib" : "AppCatalog",
+    "catalogSite": "<catalog-site-relative-path>"
+}
+
+build.task('upload-to-sharepoint', {
+	execute: (config) => {
+		return new Promise((resolve, reject) => {
+			const deployFolder = require('./config/copy-assets.json');
+			const folderLocation = `./${deployFolder.deployCdnPath}/**/*.js`;
+			
+			return gulp.src(folderLocation)
+			.pipe(spsync({
+				"username": config.production ? production.username : test.username,
+				"password": config.production ? production.password : test.password,
+				"site": `https://${config.production ? production.tenant : test.tenant}.sharepoint.com/${config.production ? production.cdnSite : test.cdnSite}`,
+				"libraryPath": config.production ? production.cdnLib : test.cdnLib,
+				"publish": true
+			}))
+			.on('finish', resolve);
+		});
+	}
+});
+
+build.task('upload-app-pkg', {
+	execute: (config) => {
+		return new Promise((resolve, reject) => {
+			const pkgFile = require('./config/package-solution.json');
+			const folderLocation = `./sharepoint/${pkgFile.paths.zippedPackage}`;
+            log('Opening...   :' + pkgFile)
+            log('Opening...   :' + folderLocation)
+			return gulp.src(folderLocation)
+			.pipe(spsync({
+				"username": config.production ? production.username : test.username,
+				"password": config.production ? production.password : test.password,
+				"site": `https://${config.production ? production.tenant : test.tenant}.sharepoint.com/${config.production ? production.catalogSite : test.catalogSite}`,
+				"libraryPath": "AppCatalog",
+				"publish": true
+			}))
+			.on('finish', resolve);
+		});
+	}
+});
+
+
+build.initialize(gulp);
